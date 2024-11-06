bd53e4597bc0452c69f9192da9d4b5983b03d876
BlazeGAI <165929771+BlazeGAI@users.noreply.github.com>
2024-11-06 16:17:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.styles import Alignment, Font, PatternFill
 
-st.title("Alumni Sheet Checker")
+st.title("Excel Assignment 10.1 Checker")
 
 # Upload Excel file
 uploaded_file = st.file_uploader("Upload your Excel file", type=["xlsx"])
